abded31b8f34ed5348d34fd92fc740c8fb52cb97
abbasikov <writeumair@gmail.com>
2018-05-27 01:38:22 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Pimax Hardware Config
+
+This application defines the configuration for the pimax-hardware
+
+## Running Locally
---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const config = require('./config');
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+    response.render('pages/index');
+});
+
+app.get('/config', function(request, response) {
+    response.json(config);
+});
+
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+});
+
+
